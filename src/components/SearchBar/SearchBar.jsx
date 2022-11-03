804d428b5440414a63a53857619361ba3f47ae1d
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-03 14:11:59 +0200

    Create SearchBar.jsx

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const SearchBar = () => (
+  <div>
+    <form className="search-form" id="search-form">
+      <input
+        type="text"
+        name="searchQuery"
+        autoComplete="off"
+        placeholder="Search images..."
+      />
+      <button type="submit">Search</button>
+    </form>
+  </div>
+);
+
+SearchBar.propTypes = {};
