dc9faabfd80be160095b478b59ab344526c06b65
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-05 00:27:55 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,24 @@
-//<ul class="gallery">
-//  <!-- Набор <li> с изображениями -->
-//</ul>
+import React from 'react';
+import { Gallery, GalleryItem, Img } from 'components/Styles/Element.styled';
+import PropTypes from 'prop-types';
+
+export const ImageGallery = ({ images }) => {
+  return (
+    <Gallery>
+      {images.map(image => (
+        <GalleryItem key={image.id}>
+          <Img src={image.webformatURL} alt={image.tags} loading="lazy" />
+        </GalleryItem>
+      ))}
+    </Gallery>
+  );
+};
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.exact({
+      tags: PropTypes.string,
+      webformatURL: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
