e2e4a064c452b0a1b948910c7a8e8faef53fa59f
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-05 00:28:16 +0200

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,22 @@
-import {
-  ButtonIcon,
-  Form,
-  Input,
-  SearchBox,
-} from 'components/Styles/Element.styled';
+import { ButtonIcon, SearchBox } from 'components/Styles/Element.styled';
 import React from 'react';
 import { BsSearch } from 'react-icons/bs';
-//import PropTypes from 'prop-types';
+import SearchForm from './SearchForm';
+import SearchInput from './SearchInput';
+import PropTypes from 'prop-types';
 
-export const SearchBar = () => (
+export const SearchBar = ({ onSubmit, onChange, value }) => (
   <SearchBox>
-    <Form id="search-form">
+    <SearchForm onSubmit={onSubmit}>
       <ButtonIcon type="submit">
         <BsSearch />
       </ButtonIcon>
-      <Input
-        type="text"
-        autocomplete="off"
-        autofocus
-        placeholder="Search images and photos"
-      />
-    </Form>
+      <SearchInput value={value} onChange={onChange} />
+    </SearchForm>
   </SearchBox>
 );
+SearchBar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
