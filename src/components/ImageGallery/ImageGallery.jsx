f25d521e6abce956915441427e5dd2e729c3111e
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-11-05 15:42:06 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Gallery, GalleryItem, Img } from 'components/Styles/Element.styled';
+import { Gallery } from 'components/Styles/Element.styled';
 import PropTypes from 'prop-types';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 export const ImageGallery = ({ images, openModal }) => {
   return (
     <Gallery>
-      {images.map(image => (
-        <GalleryItem key={image.id}>
-          <Img
-            src={image.webformatURL}
-            alt={image.tags}
-            onClick={() => openModal(image.largeImageURL)}
-            loading="lazy"
-          />
-        </GalleryItem>
-      ))}
+      <ImageGalleryItem images={images} openModal={openModal} />
     </Gallery>
   );
 };
 ImageGallery.propTypes = {
+  openModal: PropTypes.func.isRequired,
   images: PropTypes.arrayOf(
     PropTypes.exact({
       tags: PropTypes.string,
